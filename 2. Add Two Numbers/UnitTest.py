2d578c7c5dcda8ca68953c2b0119d703d9800800
Matt McGinnis <mcginnisma90@gmail.com>
2022-04-22 20:43:17 -0400

    More setup for unit tests and imported Optional from typing module

---
+++ 2. Add Two Numbers/UnitTest.py
@@ -0,0 +1,12 @@
+import unittest
+from Solution import Solution, ListNode
+
+class TestAddTwoNumbers(unittest.TestCase):
+    def test_1(self):
+        pass
+    def test_2(self):
+        pass
+    def test_3(self):
+        pass
+if __name__ == "__main__":
+    unittest.main()
