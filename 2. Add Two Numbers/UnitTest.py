8631df1070fcdf46efc7a9cbf3cd8b720dad5873
Matt McGinnis <mcginnisma90@gmail.com>
2022-04-24 20:57:00 -0400

    Updated unit tests

--- 2. Add Two Numbers/UnitTest.py
+++ 2. Add Two Numbers/UnitTest.py
@@ -3,10 +3,22 @@ from Solution import Solution, ListNode
 
 class TestAddTwoNumbers(unittest.TestCase):
     def test_1(self):
-        pass
+        l1 = ListNode(2, ListNode(4, ListNode(3, None)))
+        l2 = ListNode(5, ListNode(6, ListNode(4, None)))
+        expected = ListNode(7, ListNode(0, ListNode(8, None)))
+        actual = Solution.addTwoNumbers(self, l1, l2)
+        self.assertEqual(expected, actual)
     def test_2(self):
-        pass
+        l1 = ListNode(0, None)
+        l2 = ListNode(0, None)
+        expected = ListNode(0, None)
+        actual = Solution.addTwoNumbers(self, l1, l2)
+        self.assertEqual(expected, actual)
     def test_3(self):
-        pass
+        l1 = ListNode(9, ListNode(9, ListNode(9, ListNode(9, ListNode(9, ListNode(9, ListNode(9, None)))))))
+        l2 = ListNode(9, ListNode(9, ListNode(9, ListNode(9, None))))
+        expected = ListNode(8, ListNode(9, ListNode(9, ListNode(9, ListNode(0, ListNode(0, ListNode(0, ListNode(1, None))))))))
+        actual = Solution.addTwoNumbers(self, l1, l2)
+        self.assertEqual(expected, actual)
 if __name__ == "__main__":
     unittest.main()
