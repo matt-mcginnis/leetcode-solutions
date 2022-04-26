66c92d53ae9f37c0f5d9e98a1867746518e17970
Matt McGinnis <mcginnisma90@gmail.com>
2022-04-26 14:54:53 -0400

    Updated unit tests for actual solution

--- 2. Add Two Numbers/UnitTest.py
+++ 2. Add Two Numbers/UnitTest.py
@@ -5,7 +5,7 @@ class TestAddTwoNumbers(unittest.TestCase):
     def test_1(self):
         l1 = ListNode(2, ListNode(4, ListNode(3, None)))
         l2 = ListNode(5, ListNode(6, ListNode(4, None)))
-        expected = ListNode(7, ListNode(10, ListNode(7, None)))
+        expected = ListNode(7, ListNode(0, ListNode(8, None)))
         actual = Solution.addTwoNumbers(self, l1, l2)
         while actual or expected:
             self.assertEqual(expected.val, actual.val)
@@ -23,7 +23,7 @@ class TestAddTwoNumbers(unittest.TestCase):
     def test_3(self):
         l1 = ListNode(9, ListNode(9, ListNode(9, ListNode(9, ListNode(9, ListNode(9, ListNode(9, None)))))))
         l2 = ListNode(9, ListNode(9, ListNode(9, ListNode(9, None))))
-        expected = ListNode(18, ListNode(18, ListNode(18, ListNode(18, ListNode(9, ListNode(9, ListNode(9, None)))))))
+        expected = ListNode(8, ListNode(9, ListNode(9, ListNode(9, ListNode(0, ListNode(0, ListNode(0, ListNode(1, None))))))))
         actual = Solution.addTwoNumbers(self, l1, l2)
         while actual or expected:
             self.assertEqual(expected.val, actual.val)
