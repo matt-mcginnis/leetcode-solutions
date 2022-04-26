c94712682f0fb6dfc3f8f8b4005da9828bf8cd51
Matt McGinnis <mcginnisma90@gmail.com>
2022-04-26 16:21:38 -0400

    Updated solution for modified problem

--- 2. Add Two Numbers/Modified/Solution.py
+++ 2. Add Two Numbers/Modified/Solution.py
@@ -6,19 +6,15 @@ class ListNode:
         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        head = ListNode(l1.val + l2.val, None)
-        curr_node = head
-        next_node = None
-        while l1.next and l2.next:
-            next_node = ListNode(l1.next.val + l2.next.val)
-            curr_node.next = next_node
-            curr_node = next_node
-            l1 = l1.next
-            l2 = l2.next
-        if l1.next:
-            curr_node.next = l1.next
-        elif l2.next:
-            curr_node.next = l2.next
-        else:
-            return head
-        return head
+        dummy = curr_node = ListNode(0)
+        while l1 or l2:
+            curr_val = 0
+            if l1:
+                curr_val += l1.val
+                l1 = l1.next
+            if l2:
+                curr_val += l2.val
+                l2 = l2.next
+            curr_node.next = ListNode(curr_val)
+            curr_node = curr_node.next
+        return dummy.next
