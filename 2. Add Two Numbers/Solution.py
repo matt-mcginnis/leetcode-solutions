11e1f89fe230b16a1ec38085e461259a8cabe2aa
Matt McGinnis <mcginnisma90@gmail.com>
2022-05-08 08:52:01 -0400

    Added comments to solution

--- 2. Add Two Numbers/Solution.py
+++ 2. Add Two Numbers/Solution.py
@@ -6,6 +6,17 @@ class ListNode:
         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        '''
+        1) Create dummy node to better handle single node edge cases
+        2) Assign dummy node and curr_node variable to empty ListNode
+        3) Add carry_over variable to store anything that needs to be carried to the next digit
+        4) Iterate while there is something in l1, l2 or carry_over
+        4) Keep value of sum l1.val and l2.val in carry_over and increment l1 and l2
+        5) Add curr_node.next with value of 1's digit in carry_over variable (hence % 10)
+        6) Increment curr_node
+        7) Finally, store amount to be carried over by getting the 10's digit of carry_over (hence // 10)
+        8) Return dummy.next when loop exits
+        '''
         dummy = curr_node = ListNode(0)
         carry_over = 0
         while l1 or l2 or carry_over:
